f2a17618ba0a4a80796c6c63c2aa450832cd373e
user <user@DESKTOP-4NMVE1B>
2022-03-28 12:24:04 +0900

    findId.js

--- assets/res/www/js/ui/findId.js
+++ assets/res/www/js/ui/findId.js
@@ -40,14 +40,16 @@
       var self = this;
       var name = this.els.$userNmIpt.val().trim();
       var phone = this.els.$cellPhoneIpt.val().trim();
+      var regPhone = /^01([0|1|6|7|8|9])([0-9]{3,4})([0-9]{4})$/;
       
       if(name == '') {
         return alert('이름을 입력해주세요.');
       }
       if(phone == '') {
-        return alert('핸드폰 번호를 입력해주세요.');
-      }else{
-        return alert('핸드폰 번호는 숫자만 입력해주세요.');
+        return alert('휴대폰 번호를 입력해주세요.');
+      }
+      if(!regPhone.test(phone)) {
+        return alert('휴대폰 번호는 숫자로만 정확히 입력해주세요.');
       }
       
       MNet.sendHttp({
@@ -58,6 +60,7 @@
         },
         succ: function (data) {
           //if(data.rsltCode == '0000') {
+          
             return alert('아이디 : ' + data.loginId);
           //}
         },
