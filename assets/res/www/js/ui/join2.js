1f71af422a40064e4e925e060aa81f19492835be
user <user@DESKTOP-4NMVE1B>
2022-03-29 13:26:44 +0900

    join2.js

--- assets/res/www/js/ui/join2.js
+++ assets/res/www/js/ui/join2.js
@@ -32,27 +32,31 @@
     },
     initEvent : function initEvent(){
       var self = this;
-      module.onKeyupNum(self.els.$cellPhoneIpt);
       this.els.$nextBtn.on('click', function(){
         self.inputOk();
       });      
     },
     inputOk : function(){
       var nm = this.els.$userNmIpt.val().trim();
-      var regNm = /^[가-힣]{2,5}$/;
+      var regNm = /^[가-힣a-zA-Z]{2,}$/;
       var gender = $("input:radio[name='gender']:checked").val();
       var year = this.els.$year.val().trim();
       var month = this.els.$month.val().trim();
       var date = this.els.$date.val().trim();
+      
+      var today = new Date();
+      var todayYear = today.getFullYear(); 
+      var todayMonth = today.getMonth() + 1; 
+      var todayDate = today.getDate();
+  
       var cp = this.els.$cellPhoneIpt.val().trim();
       var regCp = /^01(?:0|1|[6-9])([0-9]{3,4})([0-9]{4})$/;
-      var birthDate	= year + module.digitNum(month) + date;
-      
+
       if(module.isEmpty(nm)){
         return alert('이름을 입력해주세요.');
       }
       if(!regNm.test(nm)){
-        return alert('이름을 정확히 입력해주세요.');
+        return alert('이름은 2자 이상의 한글 또는 영문으로만 정확히 입력해주세요.');
       }
       if(module.isEmpty(gender)){
         return alert('성별을 선택해주세요.');
@@ -60,14 +64,22 @@
       if(module.isEmpty(year)){
         return alert('생년을 입력해주세요.');
       }
-      if(year < 1700 || year > 2022){
+      if(year < 1700){
         return alert('생년을 정확히 입력해주세요.');        
+      }
+      if(todayYear < year){
+        return alert('현재보다 미래인 년은 생년에 적합하지 않습니다.');
       }
       if(module.isEmpty(month)){
         return alert('생월을 입력해주세요.');
       }
       if(month > 12 || month == 0){
         return alert('생월을 정확히 입력해주세요.');        
+      }
+      if(todayYear == year){
+        if(todayMonth < month){
+          return alert('현재보다 미래인 월은 생월에 적합하지 않습니다.');
+        }
       }      
       if(module.isEmpty(date)){
         return alert('생일을 입력해주세요.');
@@ -78,25 +90,33 @@
       if((month==4 || month==6 || month==9 || month==11) && date == 31){
         return alert(month+'월은 31일이 존재하지 않습니다.');
       }
-      if(month == 2 && date > 29){
-        return alert(month+'월은 29일까지만 존재합니다.');
-      }
-      
-      if(module.timeCheck(birthDate, 12)){
-        return alert('생년월일을 정확히 입력해주세요.');
+      if (month == 2) {
+        var noDate = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+           if (date>29 || (date==29 && !noDate)) {
+              return alert(year + '년 2월은 ' + date + '일이 존재하지 않습니다.');
+           }
+      }     
+      if(todayYear == year){
+        if(todayMonth >= month){
+          if(todayDate < date){
+            return alert('현재보다 미래인 일은 생일에 적합하지 않습니다.');
+          }
+        }
       }
       if(module.isEmpty(cp)){
         return alert('휴대폰 번호를 입력해주세요.');
       }
       if(!regCp.test(cp)){
-        return alert('휴대폰 번호를 정확히 입력해주세요.');
+        return alert('휴대폰 번호는 숫자로만 정확히 입력해주세요.');
       }
       M.page.html('./join3.html', {
         param : {
           userNm : nm,
           gender : gender,
-          birthDate :	birthDate,
-          cellPhone	: cp,
+          year : year,
+          month : month,
+          date : date,
+          cellPhone	: cp
         }
       });
     }
