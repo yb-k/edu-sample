156fa5537ddac0d79d1aabbd2d31efffa2c0c2ee
SeoungHo choi <symw512@naver.com>
2022-03-30 21:57:38 +0900

    write.js완료

--- assets/res/www/js/ui/write.js
+++ assets/res/www/js/ui/write.js
@@ -6,7 +6,10 @@
 
 // 페이지 단위모듈
 (function ($, M, MNet, config, SERVER_PATH, window) {
-  var checkId;
+  var updateCk;
+  var seqNoCk;
+  var imgNameSend;
+
   var page = {
     els: {
       $subjectIpt: null,
@@ -28,32 +31,37 @@
     init: function init() {
       this.els.$subjectIpt = $('#subject');
       this.els.$contentIpt = $('#content');
-      this.els.$file = $('#file');
+      this.els.$imgName = $('#ipt-img-name');
       this.els.$writeRegist = $('#btn-submit');
       this.els.$btnIng = $('#btn-ing');
-      this.els.$imgName = $('#ipt-img-name');
       this.els.$back = $('#back');
 
     },
     initView: function initView() {
       //화면에서 세팅할 동적데이터
       var self = this;
-      if (M.data.global('seqNoSend') != null) {
-
-
+      
+      seqNoCk =  M.data.param("seqNoNext");
+        imgNameSend =  M.data.param("imgNameSend");
+      
+      
+      if (seqNoCk) {
         MNet.sendHttp({
           path: SERVER_PATH.NOTICE_DETAIL,
           data: {
             "loginId": M.data.global('userIdSend'),
-            "seqNo": M.data.global('seqNoSend')
+            "seqNo": seqNoCk
           },
           succ: function (data) {
             console.log(data);
-            $('#subject').html(data.title);
-            $('#content').html(data.content);
-            console.log(data.imgUrl);
+            self.els.$subjectIpt.val(data.title);
+            self.els.$contentIpt.val(data.content);
+            
+
             if (data.imgUrl != null) {
-              $('#ipt-img-name').attr('src', data.imgUrl);
+              self.data.imgPath = data.imgUrl;
+              $("#ipt-img-name").val(imgNameSend);
+           
 
             }
           },
@@ -74,7 +82,13 @@
 
       var self = this;
       this.els.$writeRegist.on('click', function () {
-        self.write();
+        if(seqNoCk){
+          
+            self.update();
+        }else{
+          self.write();
+        }
+        
       })
 
       this.els.$btnIng.on('click', function () {
@@ -100,6 +114,7 @@
           if (status == 'SUCCESS') {
             self.data.imgPath = result.fullpath;
             self.els.$imgName.val(result.name);
+           
           } else {
             self.data.imgPath = null;
             self.els.$imgName.val('');
@@ -108,7 +123,101 @@
       });
     },
 
+    // 게시글 수정
+    update : function(){
+      var self = this;
+      var subjectIpt = this.els.$subjectIpt.val().trim(); // 게시글 목록 가져오기
+      var contentIpt = this.els.$contentIpt.val().trim(); // 게시글 내용  가져오기
+      var imgPath = self.data.imgPath;
+      
+      
 
+      if (subjectIpt == '') {
+        return alert('제목을 입력해주세요.');
+      }
+      if (contentIpt == '') {
+        return alert('내용을 입력해주세요.');
+      }
+     
+      
+
+
+      if (imgPath != null && imgPath != '' ) {
+        var body = [{
+            name: "file",
+            content: imgPath,
+            type: "FILE"
+          },
+          {
+            name: "content",
+            content: contentIpt,
+            type: "TEXT"
+          },
+          {
+            name: "title",
+            content: subjectIpt,
+            type: "TEXT"
+          },
+          {
+            name: "loginId",
+            content: M.data.global('userIdSend'),
+            type: "TEXT"
+          },
+          {
+            name: "seqNo",
+            content: seqNoCk,
+            type: "TEXT"
+          },
+        ]
+        // { content: "파일업로드", type: "TEXT" },
+        // { name: "imgs", content: "test.zip", type: "FILE" },
+        $.fileHttpSend({
+          path: SERVER_PATH.NOTICE_UPDATE_IMG,
+          body: body,
+          succ: function () {
+            console.log("argument");
+            M.page.replace({
+              url: './list.html',
+            });
+            var pagelist = M.info.stack();
+            M.page.remove(pagelist[1].key);  
+          },
+          progress: function () {
+            
+            console.log("argument");
+            M.page.html('./main.html');
+          },error : function(){
+            alert("에러");
+          }
+
+        })
+      } else {
+
+        MNet.sendHttp({
+          path: SERVER_PATH.NOTICE_UPDATE,
+          data: {
+            "loginId": M.data.global('userIdSend'),
+            "title": subjectIpt,
+            "content": contentIpt,
+          },
+          succ: function (data) {
+            console.log(data);
+            
+            M.page.replace({
+              url: './list.html',
+            });
+            var pagelist = M.info.stack();
+            M.page.remove(pagelist[1].key);   
+          },
+          error: function (data) {
+            console.log(data);
+            alert("게시글 수정 실패");
+            M.page.html('./write.html');
+          }
+        });
+      }
+
+    },
 
 
     // 게시글등록 
@@ -156,11 +265,11 @@
           path: SERVER_PATH.NOTICE_WRITE_IMG,
           body: body,
           succ: function () {
-            console.log("ㅊ호");
+            console.log("argument");
             M.page.html('./list.html');
           },
           progress: function () {
-            console.log("ㅍㅁㅁㅁㅁ");
+            console.log("argument");
             M.page.html('./main.html');
           },
 
@@ -176,9 +285,13 @@
           },
           succ: function (data) {
             console.log(data);
-
-            M.page.html('./list.html');
+            M.page.replace({
+              url: './list.html',
+            });
+            var pagelist = M.info.stack();
+            M.page.remove(pagelist[1].key);
           },
+           
           error: function (data) {
             console.log(data);
             alert("게시글 작성 실패");
